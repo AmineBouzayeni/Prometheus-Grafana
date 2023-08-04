33912a92602578380f442e987ea604eecce95dac
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-08-04 14:19:18 +0200

    Put IP & credentials as variables in the upload-dashboard script

--- test-upload.py
+++ test-upload.py
@@ -1,8 +1,6 @@
 from grafana_api.grafana_face import GrafanaFace
 import json
-
-grafana_api = GrafanaFace(auth=("__ADMIN_USER__", "__ADMIN_PASSWORD__"), host='__GRAFANA_IP__:__GRAFANA_PORT__')
-
+grafana_api = GrafanaFace(auth=("admin", "Grafana1717!"), host='34.140.233.235:30008')
 with open('dashboards/kube-dashboard.json') as file:
     json_dashboard = json.load(file)
 try:
@@ -11,4 +9,3 @@ try:
 except Exception as e:
     print(f"The dashboard creation failed with error: {str(e)}")
     exit(1)
-
