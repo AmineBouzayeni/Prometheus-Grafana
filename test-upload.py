ccc73114f8718e9621c789ba4fe21ac5c346ca32
Amine Bouzayeni <mohamed-amine.bouzayeni@teolia.fr>
2023-08-07 14:32:56 +0200

    Add sidecar label

--- test-upload.py
+++ test-upload.py
@@ -1,6 +1,6 @@
 from grafana_api.grafana_face import GrafanaFace
 import json
-grafana_api = GrafanaFace(auth=("admin", "Grafana1717!"), host='34.140.233.235:30008')
+grafana_api = GrafanaFace(auth=("admin", "Grafana1717!"), host='35.241.211.74:30008')
 with open('dashboards/kube-dashboard.json') as file:
     json_dashboard = json.load(file)
 try:
